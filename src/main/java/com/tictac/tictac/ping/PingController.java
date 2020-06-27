9cdd779b080499890bae3335d81f210cac26508d
Rajat.Agarwal <Rajat.Agarwal@e-zest.in>
2020-06-27 12:34:17 +0530

    init commit

---
+++ src/main/java/com/tictac/tictac/ping/PingController.java
@@ -0,0 +1,14 @@
+package com.tictac.tictac.ping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+	@GetMapping(path = "/ping")
+	public String ping() {
+		return "pong";
+
+	}
+}
